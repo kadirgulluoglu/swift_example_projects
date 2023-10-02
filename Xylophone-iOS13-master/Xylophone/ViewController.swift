6e21d948f562fd0ca282ffef59eff4acc8a2d573
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-10-02 15:29:53 +0300

    add function alpha

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -10,27 +10,32 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func onPressed(_ sender: UIButton) {
         playSound(title: sender.currentTitle ?? "A")
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     
     func playSound(title: String) {
-           let url = Bundle.main.url(forResource: title, withExtension: "wav")
-           player = try! AVAudioPlayer(contentsOf: url!)
-           player.play()
-                   
-       }
-
-
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
+    
 }
 
