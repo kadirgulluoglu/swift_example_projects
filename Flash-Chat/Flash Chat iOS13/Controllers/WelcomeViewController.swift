3534bd77ac2d61c4fd1a3dead679492c6a5310bf
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-10-21 13:28:36 +0300

    add flashchat

---
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = ""
+        let titleText = "⚡️HaydiChat"
+
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+        }
+
+    }
+
+
+}
