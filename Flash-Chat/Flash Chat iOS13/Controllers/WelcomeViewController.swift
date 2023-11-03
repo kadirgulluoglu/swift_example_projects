f6f439c968cc8ce3f00f12011a31bb25eb9e0d5f
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-11-03 09:53:54 +0300

    add ClTyping Pod

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,30 +1,16 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad ()
+    {
         super.viewDidLoad()
 
-        titleLabel.text = ""
-        let titleText = "⚡️HaydiChat"
-
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-        }
 
     }
-
-
 }
+
+
