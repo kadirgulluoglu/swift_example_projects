f188c266131bfa3a611738ea6af6e6ce3b686be9
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-12-11 15:29:54 +0300

    create File

---
+++ Messenger/Messenger/Controllers/NewConversationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationsViewController.swift
+//  Messenger
+//
+//  Created by AyzaSoft on 11.12.2023.
+//
+
+import UIKit
+
+class NewConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Messenger/Messenger/Controllers/PhotoViwerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViwerViewController.swift
+//  Messenger
+//
+//  Created by AyzaSoft on 11.12.2023.
+//
+
+import UIKit
+
+class PhotoViwerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
