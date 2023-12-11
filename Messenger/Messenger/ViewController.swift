a8540f8d66d1cfb1659a80797052bf8c5ae92561
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-12-11 15:16:05 +0300

    init project

--- Messenger/Messenger/ViewController.swift
+++ Messenger/Messenger/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by AyzaSoft on 11.12.2023.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
