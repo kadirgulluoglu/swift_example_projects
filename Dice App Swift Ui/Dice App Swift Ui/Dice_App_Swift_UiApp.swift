73c8eca5b811af83ac5869121058837d3ca87b26
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2024-01-06 10:29:16 +0300

    init dice app

---
+++ Dice App Swift Ui/Dice App Swift Ui/Dice_App_Swift_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_App_Swift_UiApp.swift
+//  Dice App Swift Ui
+//
+//  Created by AyzaSoft on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dice_App_Swift_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
